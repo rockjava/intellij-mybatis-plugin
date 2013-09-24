4e998edbed0d9e9117ce79038457110d20427558
Yanglin <insist.day@gmail.com>
2013-09-24 16:15:22 +0800

    Simply refactor GenerateMapperIntention to make code clear

--- src/com/seventh7/mybatis/intention/GenerateMapperIntention.java
+++ src/com/seventh7/mybatis/intention/GenerateMapperIntention.java
@@ -1,9 +1,7 @@
 package com.seventh7.mybatis.intention;
 
 import com.google.common.base.Function;
-import com.google.common.collect.BiMap;
 import com.google.common.collect.Collections2;
-import com.google.common.collect.HashBiMap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
@@ -31,10 +29,8 @@ import org.jetbrains.annotations.NotNull;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import java.util.Set;
 
 /**
  * @author yanglin
@@ -43,12 +39,7 @@ public class GenerateMapperIntention extends GenericIntention {
 
   @NotNull @Override
   public String getText() {
-    return "[Mybatis] Generate mapper with xml";
-  }
-
-  @Override
-  public boolean startInWriteAction() {
-    return false;
+    return "[Mybatis] Generate mapper of xml";
   }
 
   @Override @NotNull
@@ -57,10 +48,27 @@ public class GenerateMapperIntention extends GenericIntention {
   }
 
   @Override
+  public boolean startInWriteAction() {
+    return false;
+  }
+
+  @Override
   public void invoke(@NotNull final Project project, final Editor editor, PsiFile file) throws IncorrectOperationException {
     PsiElement element = file.findElementAt(editor.getCaretModel().getOffset());
-    final PsiClass clzz = PsiTreeUtil.getParentOfType(element, PsiClass.class);
-    final Map<String, PsiDirectory> pathMap = getPathMap(MapperUtils.findMapperDirectories(project));
+    PsiClass clzz = PsiTreeUtil.getParentOfType(element, PsiClass.class);
+    Collection<PsiDirectory> directories = MapperUtils.findMapperDirectories(project);
+    if (0 == directories.size()) {
+      handleChooseNewFolder(project, editor, clzz);
+    } else {
+      handleMutilDirectories(project, editor, clzz, directories);
+    }
+  }
+
+  private void handleMutilDirectories(Project project,
+                                      final Editor editor,
+                                      final PsiClass clzz,
+                                      Collection<PsiDirectory> directories) {
+    final Map<String, PsiDirectory> pathMap = getPathMap(directories);
     final ArrayList<String> keys = Lists.newArrayList(pathMap.keySet());
     ListSelectionListener popupListener = new ListSelectionListener() {
       @Override
@@ -77,21 +85,16 @@ public class GenerateMapperIntention extends GenericIntention {
     uiComponentFacade.showListPopupWithSingleClickable("Choose folder",
                                                        popupListener,
                                                        "Choose another",
-                                                       getRechooseFolderListener(editor, clzz),
-                                                       setPathText(project, keys));
+                                                       getChooseFolderListener(editor, clzz),
+                                                       getPathText(project, keys));
   }
 
-  private ClickableListener getRechooseFolderListener(final Editor editor, final PsiClass clzz) {
+  private ClickableListener getChooseFolderListener(final Editor editor, final PsiClass clzz) {
     final Project project = clzz.getProject();
     return new ClickableListener() {
       @Override
       public void clicked() {
-        UiComponentFacade uiComponentFacade = UiComponentFacade.getInstance(project);
-        VirtualFile vf = uiComponentFacade.showSingleFolderSelectionDialog("Select target folder", project.getBaseDir(), project.getBaseDir());
-        if (null != vf) {
-          PsiManager psiManager = PsiManager.getInstance(project);
-          processGenerate(editor, clzz, psiManager.findDirectory(vf));
-        }
+        handleChooseNewFolder(project, editor, clzz);
       }
 
       @Override
@@ -101,7 +104,16 @@ public class GenerateMapperIntention extends GenericIntention {
     };
   }
 
-  private String[] setPathText(Project project, Collection<String> paths) {
+  private void handleChooseNewFolder(Project project, Editor editor, PsiClass clzz) {
+    UiComponentFacade uiComponentFacade = UiComponentFacade.getInstance(project);
+    VirtualFile vf = uiComponentFacade.showSingleFolderSelectionDialog("Select target folder", project.getBaseDir(), project.getBaseDir());
+    if (null != vf) {
+      PsiManager psiManager = PsiManager.getInstance(project);
+      processGenerate(editor, clzz, psiManager.findDirectory(vf));
+    }
+  }
+
+  private String[] getPathText(Project project, Collection<String> paths) {
     final String projectBasePath = project.getBasePath();
     Collection<String> transform = Collections2.transform(paths, new Function<String, String>() {
       @Override
@@ -125,7 +137,7 @@ public class GenerateMapperIntention extends GenericIntention {
       return;
     }
     if (!directory.isWritable()) {
-      HintManager.getInstance().showInformationHint(editor, "Target directory is not writable");
+      HintManager.getInstance().showErrorHint(editor, "Target directory is not writable");
       return;
     }
     try {
@@ -135,6 +147,7 @@ public class GenerateMapperIntention extends GenericIntention {
                                                                     clzz.getName(), directory, properties);
       EditorService.getInstance(clzz.getProject()).scrollTo(psiFile, 0);
     } catch (Exception e) {
+      HintManager.getInstance().showErrorHint(editor, "Failed: " + e.getCause());
     }
   }
 
