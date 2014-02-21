27adcb3a5e734b3b9aeb51f5c51d32c4a77b1b75
Yanglin <insist.day@gmail.com>
2014-02-21 09:59:54 +0800

    Fix issue#15

--- src/com/seventh7/mybatis/dom/model/SelectKey.java
+++ src/com/seventh7/mybatis/dom/model/SelectKey.java
@@ -1,8 +1,14 @@
 package com.seventh7.mybatis.dom.model;
 
+import com.intellij.util.xml.GenericAttributeValue;
+import com.intellij.util.xml.Required;
+
 /**
  * @author yanglin
  */
 public interface SelectKey extends GroupTwo, ResultTypeGroup {
+
+  @Required(value = false)
+  public GenericAttributeValue<String> getId();
 
 }
