d58b82c660d06c904ac49e88f7a3126a46c21c71
Seventh7 <seventh7.me@gmail.com>
2014-07-18 23:55:27 +0800

    Fix bug: Range of injected sql in mapper xml

--- src/com/seventh7/mybatis/xml/SqlLanguageInjector.java
+++ src/com/seventh7/mybatis/xml/SqlLanguageInjector.java
@@ -1,41 +1,65 @@
 package com.seventh7.mybatis.xml;
 
-import com.intellij.psi.InjectedLanguagePlaces;
-import com.intellij.psi.LanguageInjector;
+import com.intellij.lang.injection.MultiHostInjector;
+import com.intellij.lang.injection.MultiHostRegistrar;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiLanguageInjectionHost;
-import com.intellij.psi.xml.XmlAttribute;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.xml.XmlTag;
+import com.intellij.psi.xml.XmlText;
 import com.intellij.sql.dialects.generic.GenericDialect;
 import com.intellij.util.xml.DomElement;
 import com.intellij.util.xml.DomUtil;
-import com.seventh7.mybatis.dom.model.Mapper;
-import com.seventh7.mybatis.util.MapperUtils;
+import com.seventh7.mybatis.dom.model.Delete;
+import com.seventh7.mybatis.dom.model.Insert;
+import com.seventh7.mybatis.dom.model.Select;
+import com.seventh7.mybatis.dom.model.Update;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author yanglin
  */
-public class SqlLanguageInjector implements LanguageInjector {
+public class SqlLanguageInjector implements MultiHostInjector {
 
-  @Override
-  public void getLanguagesToInject( @NotNull PsiLanguageInjectionHost host, @NotNull InjectedLanguagePlaces injectionPlacesRegistrar) {
-    PsiElement context = host.getContext();
-    if (context == null || !MapperUtils.isElementWithinMybatisFile(context)) {
-      return;
-    }
-    DomElement domElement = DomUtil.getDomElement(context);
-    if (domElement == null) {
-      return;
-    }
-    Mapper mapper = DomUtil.getParentOfType(domElement, Mapper.class, true);
-    if (mapper == null) {
-      return;
+    private static final Class[] SUPPORTED_TYPES = new Class[]{Select.class, Insert.class, Update.class, Delete.class};
+
+    @Override
+    public void getLanguagesToInject(@NotNull MultiHostRegistrar registrar, @NotNull PsiElement context) {
+        if (!context.isValid() || !(context instanceof XmlText)) {
+            return;
+        }
+
+        XmlTag xmlTag = PsiTreeUtil.getParentOfType(context, XmlTag.class, true);
+        DomElement domElement = DomUtil.getDomElement(xmlTag);
+        if (!isSupportedType(domElement)) {
+            return;
+        }
+        registrar.startInjecting(GenericDialect.INSTANCE)
+                 .addPlace(null, null, (PsiLanguageInjectionHost) context, TextRange.create(0, context.getTextLength()));
     }
 
-    if (context instanceof XmlAttribute) {
-      injectionPlacesRegistrar.addPlace(GenericDialect.INSTANCE, ((XmlAttribute) context).getValueTextRange(), null, null);
-    }
-  }
+    private static boolean isSupportedType(DomElement element) {
+        if (element == null) {
+            return false;
+        }
 
+        for (Class clazz : SUPPORTED_TYPES) {
+            if (clazz.isInstance(element)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @NotNull
+    @Override
+    public List<? extends Class<? extends PsiElement>> elementsToInjectIn() {
+        return Collections.singletonList(XmlText.class);
+    }
 }
